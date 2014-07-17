8a955e6931c2f3e5282917430fbbd72e8ec8734c
fafafa <test2@github.github>
2014-07-17 09:55:46 +0300

    Security in progress

---
+++ src/main/java/org/springframework/security/samples/config/MessageSecurityWebApplicationInitializer.java
@@ -0,0 +1,6 @@
+package org.springframework.security.samples.config;
+
+import org.springframework.security.web.context.*;
+
+public class MessageSecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+}
